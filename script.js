6908a602db53450e183f09f8d69e0f8dc167279e
unknown <Baxtiyorddushaev@gmail.com>
2023-09-29 16:37:14 +0500

    modified

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', function () {
-        const header = document.querySelector('header');
-        header.classList.toggle('sticky', window.scrollY > 0)
+        const nav = document.querySelector('.nav');
+        nav.classList.toggle('sticky', window.scrollY > 0)
     })
 
 
